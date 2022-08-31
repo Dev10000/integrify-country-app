cabee565f8fce5bdfdcbe94ea5f7db4fd4f1f203
Tim <16493467+Dev10000@users.noreply.github.com>
2022-08-31 22:40:46 +0300

    Implement Table - WIP
    - Basic mui table setup
    - Work in process

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import Table from './components/Table';
 
 export default function App() {
   return (
     <div>
       <h1>Lets build Countries App</h1>
+      <Table />
     </div>
   );
 }
---
+++ src/components/Table.tsx
@@ -0,0 +1,80 @@
+import React, { useMemo } from 'react';
+import { Avatar, Box, Typography } from '@mui/material';
+import { DataGrid, GridColDef } from '@mui/x-data-grid';
+import moment from 'moment';
+
+const rowData = [
+  {
+    id: '1',
+    name: 'Test Name',
+    email: 'test@email.com',
+    password: '12345',
+    photoURL: '',
+    createdAt: '2022-03-10T11:51:46.607+00:00',
+    active: true,
+    role: 'basic',
+  },
+];
+
+function Table() {
+  const columns: GridColDef[] = useMemo(
+    () => [
+      {
+        field: 'photoURL',
+        headerName: 'Avatar',
+        width: 60,
+        renderCell: (params) => <Avatar src={params.row.photoURL} />,
+        sortable: false,
+        filterable: false,
+      },
+      { field: 'name', headerName: 'Nane', width: 170 },
+      { field: 'email', headerName: 'Email', width: 200 },
+      {
+        field: 'role',
+        headerName: 'Role',
+        width: 100,
+        type: 'singleSelect',
+        valueOptions: ['basic', 'editor', 'admin'],
+        editable: true,
+      },
+      {
+        field: 'active',
+        headerName: 'Active',
+        width: 100,
+        type: 'boolean',
+        editable: true,
+      },
+      {
+        field: 'createdAt',
+        headerName: 'Created At',
+        width: 200,
+        renderCell: (params) =>
+          moment(params.row.createdAt).format('YYY-MM-DD HH:MM:SS'),
+      },
+      { field: 'id', headerName: 'Id', width: 220 },
+    ],
+    []
+  );
+
+  return (
+    <Box
+      sx={{
+        height: 400,
+        width: '100%',
+      }}>
+      <Typography
+        variant="h3"
+        component="h3"
+        sx={{ textAling: 'center', mt: 3, mb: 3 }}>
+        Countries List
+      </Typography>
+      <DataGrid
+        columns={columns}
+        rows={rowData}
+        getRowId={(row: any) => row.id}
+      />
+    </Box>
+  );
+}
+
+export default Table;
